2d5a30319674f4131c8f55f8910c8ade54185e05
Stav <stav2233@gmail.com>
2025-07-04 19:15:29 +0300

    push

--- db_py_3.7.py
+++ db_py_3.7.py
@@ -1,17 +1,32 @@
 import sqlite3
 
-conn = sqlite3.connect('db1.db')
+#  conn = sqlite3.connect('db1.db')
 
+#create in memory
+#erase after program exit
+conn = sqlite3.connect(':memory:')
 
 cursor=conn.cursor()
-
 cursor.execute('''
-INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) 
-VALUES (1, 'Paul', 32, 'California', 20000.00 ), 
-(2, 'Allen', 25, 'Texas', 15000.00 ),
-(3, 'Teddy', 23, 'Norway', 20000.00 ), 
-(4, 'Mark', 25, 'Rich-Mond ', 65000.00 ),
-(5, 'David', 27, 'Texas', 85000.00 ),
-(6, 'Kim', 22, 'South-Hall', 45000.00 ); 
+CREATE TABLE IF NOT EXISTS COMPANY(
+    ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    AGE INT NOT NULL,
+    ADDRESS CHAR(50),
+    SALARY REAL
+);
 ''')
 
+cursor.execute('''
+INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES
+(1, 'Paul', 32, 'California', 20000.00 );
+(2, 'Allen', 25, 'Texas', 15000.00 );
+(3, 'Teddy', 23, 'Norway', 20000.00 );
+(4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+(5, 'David', 27, 'Texas', 85000.00 );
+(6, 'Kim', 22, 'South-Hall', 45000.00 );
+''')
+
+conn.commit() #write changes
+conn.close()  #close for safty
