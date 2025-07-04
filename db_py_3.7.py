d46d3f8c0f2fac95cd7c2c372c1d071734f97e0e
Stav <stav2233@gmail.com>
2025-07-04 20:29:29 +0300

    push

--- db_py_3.7.py
+++ db_py_3.7.py
@@ -17,6 +17,20 @@ CREATE TABLE IF NOT EXISTS COMPANY(
 ''')
 
 # cursor.execute('''
+# INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+# VALUES
+# (1, 'Paul', 32, 'California', 20000.00 );
+# (2, 'Allen', 25, 'Texas', 15000.00 );
+# (3, 'Teddy', 23, 'Norway', 20000.00 );
+# (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+# (5, 'David', 27, 'Texas', 85000.00 );
+# (6, 'Kim', 22, 'South-Hall', 45000.00 );
+# ''')
+
+cursor.executemany('''
+
+''')
+# cursor.execute('''
 # INSERT INTO if not exist COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
 # VALUES
 # (1, 'Paul', 32, 'California', 20000.00 );
